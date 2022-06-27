bdb18eab67aa535c90921eeeb628fbb59ab198ae
sloika <herjunorahn@gmail.com>
2022-06-27 15:38:16 +0200

    First commit

---
+++ Day1of100.py
@@ -0,0 +1,36 @@
+#print("Hi, what's your name?" + "\nJuno")
+# Fix the code below 👇
+
+'''
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the " + " sign.")
+print("e.g. print(\"Hello\" + \"world\")")
+print('e.g. print("Hello" + "world")')
+print("New lines can be created with a backslash and n.")
+
+
+name = input("What's your name?\n")
+print(len(name))
+
+
+#Input Switcher
+a = input("a : ")
+b = input("b : ")
+
+def inputSwitcher(x,y):
+    x=b
+    y=a
+    return x,y
+
+c,d = inputSwitcher(a,b)
+
+print("Input -> a = {0}  | b = {1}".format(a,b))
+print("Output -> a = {0}  | b = {1}".format(c,d))
+'''
+
+#Band Name Generator
+print("Hello, welcome to the ultimate Band Name Generator")
+syntax1 = input("Please name a city that is memorable for you : \n")
+syntax2 = input("Now, name your favourite doggo name : \n")
+print("Now here is the your new band name : \n" + syntax1 + " " + syntax2)
+
